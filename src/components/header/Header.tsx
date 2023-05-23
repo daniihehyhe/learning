ec6b3e08bd675603e36b37dc8af80ea03307e397
daniihehyhe <115088264+daniihehyhe@users.noreply.github.com>
2023-05-23 16:39:43 +0600

    fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+
+
 
 export const Header = () => {
   return (
-	<div>Header</div>
-  )
+	<div>Header
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, culpa. Adipisci ut, recusandae quae ratione, eligendi ex porro sapiente itaque, doloremque magnam officiis saepe. Quaerat quis omnis perferendis suscipit. Ea!</p>
+		<div className="decoration-slice">asd</div>
+	</div>
+	)
 }
