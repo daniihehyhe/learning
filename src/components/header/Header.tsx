ed3adcc388529445a4d30e4a17e3011e653efe42
daniihehyhe <115088264+daniihehyhe@users.noreply.github.com>
2023-05-23 17:12:47 +0600

    fex ts config

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,7 @@
-
-
+import React from 'react'
 
 export const Header = () => {
   return (
-	<div>Header
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, culpa. Adipisci ut, recusandae quae ratione, eligendi ex porro sapiente itaque, doloremque magnam officiis saepe. Quaerat quis omnis perferendis suscipit. Ea!</p>
-		<div className="decoration-slice">asd</div>
-	</div>
-	)
+	<div>Header</div>
+  )
 }
