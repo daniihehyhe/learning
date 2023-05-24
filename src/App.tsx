d206e3e7dfbc52252fc14e387e8a8e9766bbdf23
Azat <ergeshbaevazat@gmail.com>
2023-05-24 20:11:55 +0600

    feat: main component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { CoursesLanding } from './pages/CoursesLanding'
-//import './App.css'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <>
-    <CoursesLanding/>
-     <h1 className='bg-red-400 text-3xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis doloribus voluptates quasi aliquid provident consequatur fugiat perferendis maxime iure excepturi molestiae in cumque facilis animi deserunt, sed facere optio nam!</h1>
+      <CoursesLanding/>
     </>
   )
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,9 @@
+import React from 'react'
 
-
-
-export const Header = () => {
+function Header() {
   return (
-	<div>Header
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, culpa. Adipisci ut, recusandae quae ratione, eligendi ex porro sapiente itaque, doloremque magnam officiis saepe. Quaerat quis omnis perferendis suscipit. Ea!</p>
-		<div className="decoration-slice">asd</div>
-	</div>
-	)
+	<div className='container'>Header</div>
+  )
 }
+
+export default Header
