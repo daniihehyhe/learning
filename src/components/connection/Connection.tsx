eadb96b33d2445f9c1d8dadd863713df0d8d92c8
Aliev1029 <bturaruulu@gmail.com>
2023-05-29 09:52:09 +0600

    fix

--- src/components/connection/Connection.tsx
+++ src/components/connection/Connection.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
 import { useTranslation } from "react-i18next";
 
 
@@ -8,12 +8,13 @@ import slack from './assets/slack.png';
 import netflix from './assets/netflix.png';
 import heroku from './assets/heroku.png';
 import airbnb from './assets/airbnb.png';
-
+import laptop from './assets/laptop.png';
+import line from './assets/line.png';
 
 function Connection() {
 
-    const { t } = useTranslation();
- 
+  const { t } = useTranslation();
+
 
 
   return (
@@ -22,7 +23,7 @@ function Connection() {
       <div className='text-center my-20 px-16'>
         <h1 className=' text-5xl my-5 text-center font-bold max-sm:text-3xl'>{t("Connection.title")}</h1>
         <p className=' text-base my-5 text-gray-500'>Quisque aliquet, libero consequat elementum convallis.</p>
-        <div className='flex py-5 w-full justify-center gap-[85px] max-[1268px]:gap-[55px] max-[912px]: flex-wrap'>
+        <div className='flex py-5 w-full justify-center gap-[85px] max-[1268px]:gap-[25px] max-[912px]: flex-wrap'>
           <img src={spotify} alt="logo" className='' />
           <img src={slack} alt="logo" className='' />
           <img src={netflix} alt="logo" className='' />
@@ -32,9 +33,39 @@ function Connection() {
         </div>
       </div>
 
+      <div className='px-[144px] flex flex-col gap-[80px] py-[80px] w-[100%] max-[1120px]:gap-[20px] max-[1000px]:px-[80px] max-[600px]:py-[20px] max-[470px]:px-[40px]'>
+        <p className='text-[12px] text-orange h-5 font-semibold'>Nullam accumsan lorem in dui.</p>
+        <div className='flex gap-[200px] max-[1500px]:gap-[140px] max-[1400px]:gap-[70px]'>
+          <img className='rounded-[20px] max-[1300px]:w-[366px] max-[1300px]:h-[521px] max-[1120px]:w-[266px] max-[1120px]:h-[421px] max-[950px]:hidden' src={laptop} alt="" />
+          <div>
+            <h1 className='text-[36px] font-bold mt-[110px] leading-[34px] w-[564px] max-[1300px]:mt-[50px] max-[1300px]:text-[30px] 
+            max-[1120px]:mt-0 max-[950px]:text-[36px] max-[700px]:text-[30px] max-[600px]:text-[24px] max-[600px]:w-[335px] '>
+              Phasellus gravida semper nisi. Vestibulum rutrum, mi
+            </h1>
+            <p className=' mt-[24px] text-[16px] font-normal text-gray-500 leading-[28px] w-[564px] max-[1200px]:w-[400px] max-[950px]:w-[564px] max-[700px]:w-[400px] max-[600px]:w-[335px]'>
+              Curabitur a felis in nunc fringilla tristique. Fusce egestas elit eget
+              lorem. Etiam vitae tortor. Class aptent taciti sociosqu ad litora
+              torquent per conubia nostra, per inceptos hymenaeos.
+            </p>
+            <div className='flex gap-[80px] mt-[80px] max-[1150px]:gap-[20px] max-[950px]:gap-[80px] max-[700px]:gap-[20px] max-[600px]:flex-col'>
+              <div>
+                <img src={line} alt="" />
+                <p className=' w-[230px] text-[14px] font-normal text-gray-500 leading-[26px] mt-[24px] max-[600px]:w-[335px]'>
+                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+                </p>
+              </div>
+              <div>
+                <img src={line} alt="" />
+                <p className=' w-[230px] text-[14px] font-normal text-gray-500 leading-[26px] mt-[24px] max-[600px]:w-[335px]'>
+                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
     </section>
-
   )
 }
 
